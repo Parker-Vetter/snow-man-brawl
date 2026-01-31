f7f72d8303494ebc4dd941b51d3055f271f8fdb8
Parker Vetter <parkervetter@gmail.com>
2026-01-31 15:17:15 -0600

    scene manager and peggle change

---
+++ PeggleDrop/UI/ui_fight.gd
@@ -0,0 +1,18 @@
+extends Button
+
+var scene_manager: SceneManager
+var peggle_drop: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	scene_manager = get_tree().get_nodes_in_group("SceneManager")[0]
+	peggle_drop = get_tree().get_nodes_in_group("PeggleDrop")[0]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	scene_manager.switch_scene(peggle_drop, "res://MainGame/Scenes/mainGame.tscn", peggle_drop.game_data)
--- SceneManager/scene_manager.gd
+++ SceneManager/scene_manager.gd
@@ -1,8 +1,13 @@
+class_name SceneManager
 extends Node
 
+@onready var switch_node: Node = $SwitchNode
+
 ## Pass in the old scene(self) and the new scene that we want example of new_scene = "res://2DRTS/2D_RTS.tscn"
-func switch_scene(old_loaded_scene: Node, new_scene: String):
+func switch_scene(old_loaded_scene: Node, new_scene: String, game_data: GameData):
 	old_loaded_scene.queue_free()
 	var main_scene = load(new_scene).instantiate()
-	self.add_child.call_deferred(main_scene)
+	switch_node.add_child.call_deferred(main_scene)
 	await main_scene.ready
+	if main_scene.has_method("load_game_data") && game_data != null:
+		main_scene.load_game_state(game_data)
