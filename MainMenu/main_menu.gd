b6ceea3f3f7abb558dffb1faf561b4e35e90e435
EverybodyMakesFsteaks <130267104+EverybodyMakesFsteaks@users.noreply.github.com>
2026-02-01 12:03:51 -0600

    Button now works for special thanks

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -6,6 +6,7 @@ extends Control
 @onready var credittext:Node = $VBoxContainer2/VBoxContainer/HBoxContainer2/Button/Label
 @onready var backtext:Node = $VBoxContainer2/VBoxContainer/HBoxContainer2/Button/Back
 @onready var credits: Node = $Credits
+@onready var spthank : Node = $Spthanks
 func startGame():
 	scene_manager.switch_scene(self, "res://MainGame/Scenes/mainGame.tscn", game_data)
 	#scene_manager.switch_scene(self, "res://PeggleDrop/PeggleDrop.tscn", game_data)
@@ -15,8 +16,10 @@ func startGame():
 func _on_play_pressed() -> void:
 	startGame()
 
-
+#When credit button is pressed, swap visibility of credits and special thanks. Default visibility is not visible.
 func _on_button_pressed() -> void:
 	credittext.visible = !credittext.visible
 	backtext.visible = !backtext.visible
 	credits.visible = !credits.visible
+	spthank.visible = !spthank.visible
+	
