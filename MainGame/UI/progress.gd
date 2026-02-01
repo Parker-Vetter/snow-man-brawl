ae2e09d0314df949d0f52e17ca2c2b32acc074cc
Parker Vetter <parkervetter@gmail.com>
2026-01-31 21:54:36 -0600

    added progress bar and font and end condition for main game

---
+++ MainGame/UI/progress.gd
@@ -0,0 +1,18 @@
+extends ProgressBar
+
+@onready var main_game: MainGame = $"../../../.."
+@onready var time_left: Timer = $"../../../../TimeLeft"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	time_left.start(main_game.gameData.game_length)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	self.max_value = main_game.gameData.game_length
+	self.value = time_left.time_left
+
+
+func _on_time_left_timeout() -> void:
+	main_game.switchToPeggleDrop()
