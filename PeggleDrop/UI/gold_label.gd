aae0f185134a1b65834dcf0e37ca38d087f100ba
Archimagus <excalibr8@yahoo.com>
2026-02-01 17:52:32 -0600

    Mask Updates

--- PeggleDrop/UI/gold_label.gd
+++ PeggleDrop/UI/gold_label.gd
@@ -10,4 +10,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	self.text = "Gold: " + str(peggle_drop.game_data.gold)
+	self.text = str(peggle_drop.game_data.gold)
