88191d2e62839de1dcd24155b17aebf25e777d73
Archimagus <excalibr8@yahoo.com>
2026-01-31 20:11:07 -0600

    Start at game rather than pachinko

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -2,10 +2,10 @@ extends Control
 @onready var scene_manager: SceneManager = get_tree().get_first_node_in_group("SceneManager")
 
 
-func switchToPeggleDrop():
-	scene_manager.switch_scene(self, "res://PeggleDrop/PeggleDrop.tscn")
+func startGame():
+	scene_manager.switch_scene(self, "res://MainGame/Scenes/mainGame.tscn")
 	pass
 
 
 func _on_play_pressed() -> void:
-	switchToPeggleDrop()
+	startGame()
