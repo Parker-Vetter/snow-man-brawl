b7254920ffcf22cbd7120631da5e5ad7bfaf7886
EverybodyMakesFsteaks <130267104+EverybodyMakesFsteaks@users.noreply.github.com>
2026-01-31 23:47:34 -0600

    Credit button coding

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -2,6 +2,10 @@ extends Control
 @onready var scene_manager: SceneManager = get_tree().get_first_node_in_group("SceneManager")
 @export var game_data:GameData;
 
+#Prepare Nodes I want visible and invisible
+@onready var credittext:Node = $VBoxContainer2/VBoxContainer/HBoxContainer2/Button/Label
+@onready var backtext:Node = $VBoxContainer2/VBoxContainer/HBoxContainer2/Button/Back
+@onready var credits: Node = $Credits
 func startGame():
 	scene_manager.switch_scene(self, "res://MainGame/Scenes/mainGame.tscn", game_data)
 	#scene_manager.switch_scene(self, "res://PeggleDrop/PeggleDrop.tscn", game_data)
@@ -10,3 +14,9 @@ func startGame():
 
 func _on_play_pressed() -> void:
 	startGame()
+
+
+func _on_button_pressed() -> void:
+	credittext.visible = !credittext.visible
+	backtext.visible = !backtext.visible
+	credits.visible = !credits.visible
